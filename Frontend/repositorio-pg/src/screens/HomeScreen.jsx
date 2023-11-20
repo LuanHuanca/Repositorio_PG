1c4015cd8ba948eecd79dc973b33dd89297f749b
Luan Huanca <luanhuancam@gmail.com>
2023-11-20 09:34:56 -0400

    avance en la pantalla home y arreglo de referencias

--- Frontend/repositorio-pg/src/screens/HomeScreen.jsx
+++ Frontend/repositorio-pg/src/screens/HomeScreen.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Appbar from '../components/organismos/Appbar'
 import TituloImagen from '../components/moleculas/TituloImagen'
+import TituloContenido from '../components/moleculas/TituloContenido'
+import './HomeScreen.css'
+import CarouselTarjetas from '../components/moleculas/CarouselTarjetas'
+import Footer from '../components/organismos/Footer'
 
 const HomeScreen = () => {
   return (
     <div>
       <Appbar/>
       <TituloImagen/>
+      <TituloContenido valor={"RECIENTE"}/>
+      <CarouselTarjetas/>
+      <TituloContenido valor={"DESTACADO"}/>
+      <div className='destacado-container'>
+        <CarouselTarjetas/>
+      </div>
+      <Footer/>
     </div>
   )
 }
