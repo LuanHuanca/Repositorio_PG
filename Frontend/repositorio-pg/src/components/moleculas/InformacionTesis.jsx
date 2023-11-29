36e705220ada8f70cad30e578d8ec177b69d3090
Luan Huanca <luanhuancam@gmail.com>
2023-11-29 01:15:40 -0400

    Correciones en los envios de datos

--- Frontend/repositorio-pg/src/components/moleculas/InformacionTesis.jsx
+++ Frontend/repositorio-pg/src/components/moleculas/InformacionTesis.jsx
@@ -7,9 +7,10 @@ import { useFetch } from '../../useFetch'
 
 const InformacionTesis = () => {
   const location = useLocation();
-  const titulo = location.state?.titulo || {}; // Accede a los datos desde la ubicación
+  const searchParams = new URLSearchParams(location.search);
+  const proyecto = searchParams.get("proyecto");
 
-  const baseURL = `http://localhost:3000/busquedaPorTitulo?titulo=${titulo}`;
+  const baseURL = `http://localhost:3000/busquedaPorTitulo?titulo=${proyecto}`;
   const { data, loading, error } = useFetch(baseURL, "busquedaPorTitulo");
 
   if (loading) {
@@ -22,17 +23,17 @@ const InformacionTesis = () => {
 
   return (
     <div className='InformacionTesis-container'>
-        <h1>{data && data.TítuloDelProyecto}</h1>
+        <h1>{data[0].TítuloDelProyecto}</h1>
         <div className='Information-container'>
             <img src='src/assets/imagen de tesis.png' alt="Portada Tesis" />
             <div className='informacion'>
-              <p>Resumen: {data.Resumen}</p>
-              <p>Gestion: {data.Gestión}</p>
-              <p>Palabras Clave: {data.PalabrasClave}</p>
+              <p>Resumen: {data[0].Resumen}</p>
+              <p>Gestion: {data[0].Gestión}</p>
+              <p>Palabras Clave: {data[0].PalabrasClave}</p>
               {/* <p>Abstract: {tarjeta.Abstract}</p> */}
-              <p>Autores: {data.Autor}</p>
-              <p>Carrera: {data.Carrera}</p>
-              <p>Tutor: {data.Tutor}</p>
+              <p>Autores: {data[0].Autor}</p>
+              <p>Carrera: {data[0].Carrera}</p>
+              <p>Tutor: {data[0].Tutor}</p>
               <OurButton Texto={"Descargar"}/>
             </div>
         </div>
--- Frontend/repositorio-pg/src/components/moleculas/TarjetasTesis.jsx
+++ Frontend/repositorio-pg/src/components/moleculas/TarjetasTesis.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import './TarjetasTesis.css'
 import { NavLink, useNavigate } from "react-router-dom";
 
 
 const TarjetasTesis = ({titulo, autor, fecha, carrera}) => {
   const navigate = useNavigate();
-
+  const enviarKey = (proyecto) => {
+    navigate(`/tarjetaTesis?proyecto=${proyecto}`);
+    console.log(proyecto);
+  };
   return (
     <div className="tarjetas-de-tesis">
       <img src="/src/assets/imagen de tesis.png" alt="Portada Tesis" />
@@ -15,8 +18,8 @@ const TarjetasTesis = ({titulo, autor, fecha, carrera}) => {
         <h3>fecha publicada: {fecha}</h3>
         <h3>carrera: {carrera}</h3>
         <div className="boton-container">
-          <button>
-            <NavLink to={{ pathname: '/tarjetaTesis', state: { titulo: titulo } }}>Ingresar</NavLink>
+          <button onClick={() => enviarKey(titulo)}>
+            Ingresar
           </button>
         </div>
       </div>
