3c1a778b486bb3c3f70d70e894ad49b80af0399d
Luan Huanca <luanhuancam@gmail.com>
2023-12-02 20:25:04 -0400

    arreglo de npm

--- Frontend/repositorio-pg/src/components/moleculas/TarjetaHome.jsx
+++ Frontend/repositorio-pg/src/components/moleculas/TarjetaHome.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link,NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './TarjetaHome.css';
 
 const TarjetaHome = ({ imagen, imagenDescrip, titulo, Descripcion, fecha }) => {
