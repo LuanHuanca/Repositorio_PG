e69aa13a05d2a926e0a8da7d1a509294bed62a19
Luan Huanca <luanhuancam@gmail.com>
2023-12-05 22:42:55 -0400

    intento con deploy remoto

--- Frontend/repositorio-pg/src/api/proyectosApi.jsx
+++ Frontend/repositorio-pg/src/api/proyectosApi.jsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const proyectosApi = axios.create({
     //Aqui entregamos la url de la api, para su propio consumo en el frontend
-    baseURL: 'http://172.18.0.125:3000'
+    baseURL: 'http://192.168.0.12:3000'
 })
